e9b4dbbea1a1973b57bddf66ad6b4f62e1678623
Rodrigo M. G <rodrigo.guimaraes@kestraa.com.br>
2020-08-25 09:43:57 -0300

    ajuste de url-api para front no openshift

--- adaFront/src/app/services/url.api.ts
+++ adaFront/src/app/services/url.api.ts
@@ -1,2 +1,1 @@
-//export const URL_API = 'http://ada-api-crud-shop.apps.ca-central-1.starter.openshift-online.com' ;
-export const URL_API = 'http://localhost:8080' ;
+export const URL_API = 'http://ada-api-crud-shop.apps.ca-central-1.starter.openshift-online.com' ;
